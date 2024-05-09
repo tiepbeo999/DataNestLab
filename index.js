d40a3fe69a36ae9c80ff4cc15b2dff4bb653979b
tiepbeo999 <hamartia23pun0suy@hotmail.com>
2024-05-09 18:43:36 +0700

    demo

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
-function uniquePathsWithObstacles(obstacleGrid) {
-  const m = obstacleGrid.length;
-  const n = obstacleGrid[0].length;
-  const dp = Array.from(Array(m), () => Array(n).fill(0));
-  if (obstacleGrid[0][0] === 0) dp[0][0] = 1;
-  for (let i = 0; i < m; i++) {
-    for (let j = 0; j < n; j++) {
-      if (obstacleGrid[i][j] === 1) continue;
-      if (i > 0) dp[i][j] += dp[i - 1][j];
-      if (j > 0) dp[i][j] += dp[i][j - 1];
+function levelOrderBottom(root) {
+  if (!root) return [];
+  const result = [];
+  const queue = [root];
+  while (queue.length) {
+    const levelSize = queue.length;
+    const currentLevel = [];
+    for (let i = 0; i < levelSize; i++) {
+      const node = queue.shift();
+      currentLevel.push(node.val);
+      if (node.left) queue.push(node.left);
+      if (node.right) queue.push(node.right);
     }
+    result.unshift(currentLevel);
   }
-  return dp[m - 1][n - 1];
+  return result;
 }
