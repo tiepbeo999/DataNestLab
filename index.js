c37b593d3da98e1609eecec8d69309bb89bcce67
tiepbeo999 <hamartia23pun0suy@hotmail.com>
2024-05-11 11:29:23 +0700

    aptos sdk commit

--- index.js
+++ index.js
@@ -1,15 +1,38 @@
-function merge(intervals) {
-  intervals.sort((a, b) => a[0] - b[0]);
-  const merged = [];
-  for (const interval of intervals) {
-    if (!merged.length || merged[merged.length - 1][1] < interval[0]) {
-      merged.push(interval);
-    } else {
-      merged[merged.length - 1][1] = Math.max(
-        merged[merged.length - 1][1],
-        interval[1],
-      );
+function longestIncreasingPath(matrix) {
+  if (matrix.length === 0) return 0;
+  const rows = matrix.length;
+  const cols = matrix[0].length;
+  const dp = Array.from(Array(rows), () => Array(cols).fill(0));
+  let longestPath = 0;
+  for (let i = 0; i < rows; i++) {
+    for (let j = 0; j < cols; j++) {
+      longestPath = Math.max(longestPath, dfs(matrix, i, j, dp));
     }
   }
-  return merged;
+  return longestPath;
+  function dfs(matrix, i, j, dp) {
+    if (dp[i][j] !== 0) return dp[i][j];
+    const dirs = [
+      [0, 1],
+      [0, -1],
+      [1, 0],
+      [-1, 0],
+    ];
+    let maxPath = 1;
+    for (const dir of dirs) {
+      const newRow = i + dir[0];
+      const newCol = j + dir[1];
+      if (
+        newRow >= 0 &&
+        newRow < matrix.length &&
+        newCol >= 0 &&
+        newCol < matrix[0].length &&
+        matrix[newRow][newCol] > matrix[i][j]
+      ) {
+        maxPath = Math.max(maxPath, 1 + dfs(matrix, newRow, newCol, dp));
+      }
+    }
+    dp[i][j] = maxPath;
+    return maxPath;
+  }
 }
